6c16e7400302a571be659153d25b06f23e0d2327
Da Silva DS.BAT <dasilva.elec@gmail.com>
2026-04-06 15:11:58 +0200

    Noindex GitHub Pages - SEO dsbat.fr

--- image/convert.php
+++ image/convert.php
@@ -86,6 +86,7 @@ if (isset($_GET['scan'])) {
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+<meta name="robots" content="noindex, nofollow">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DS.BAT - Gestionnaire d'images</title>
     <style>
